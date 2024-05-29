31874b9055bebe744ce2eec85b393d70b81db42d
killameow69 <killameow69@gmail.com>
2024-05-29 22:14:21 +0530

    Add files via upload

---
+++ systic timer Core/Src/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+
+
+#define GPIOAEN				   (1U<<0)
+#define PIN5				   (1U<<5)
+
+#define LED				   		PIN5
+
+#define SYSTICK_LOAD_VAL			16000
+#define	CTRL_ENABLE					(1U<<0)
+#define CTRL_CLKSRC					(1U<<2)
+#define CTRL_COUNTFLAG				(1U<<16)
+
+void systickDelayMs(int delay);
+int main(void)
+{
+
+
+	/*1.Enable clock access to GPIOA*/
+	RCC->AHB1ENR |= GPIOAEN;
+
+	/*2.Set PA5 as output pin*/
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &=~(1U<<11);
+
+
+	while(1)
+	{
+
+
+		printf("A second passed !! \n\r");
+		GPIOA->ODR ^=LED;
+		systickDelayMs(1000);
+	}
+
+}
+
+void systickDelayMs(int delay)
+{
+	/*Reload with number of clocks per millisecond*/
+	SysTick->LOAD	= SYSTICK_LOAD_VAL;
+
+	/*Clear systick current value register */
+	SysTick->VAL = 0;
+
+	/*Enable systick and select internal clk src*/
+	SysTick->CTRL = CTRL_ENABLE | CTRL_CLKSRC;
+
+	for(int i=0; i<delay ; i++){
+
+		/*Wait until the COUNTFLAG is set*/
+
+		while((SysTick->CTRL &  CTRL_COUNTFLAG) == 0){}
+
+	}
+
+	SysTick->CTRL = 0;
+
+}
+
+
+
